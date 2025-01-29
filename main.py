af427ec11b6246e1ec55d9b6a6f075626ba1f703
YuHe123456 <q15yhe@wgsf.net>
2025-01-29 13:00:11 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -4,6 +4,10 @@ import math, random, time, pickle
 screenSize(1000,1000)
 setBackgroundColour("black")
 
+mass_multiplier = 0.5
+
+min_speed = 1
+max_speed = 50
 
 class Creature:
     def __init__(self,x,y,image,  size):
@@ -11,10 +15,30 @@ class Creature:
         self.y = y
         self.size = size   # size is a percentage of the full size image
         self.sprite = makeSprite(image)
+        self.direction = random.randint(0,359)
+        self.speed = random.randint(min_speed, max_speed)
         moveSprite(self.sprite,self.x,self.y,centre=True)
         transformSprite(self.sprite, 0, self.size/100)
         showSprite(self.sprite)
+    
+        
+        
+        
+        
+        
 
+class Player(Creature):
+    def __init__(self):
+        super().__init__()
+        self.direction = 0
+        
+    def move(self):
+        
+        self.y += math.sin(math.radians(self.direction)) * self.speed
+        self.x += math.cos(math.radians(self.direction)) * self.speed
+        
+        
+    
 
 setAutoUpdate(False)
 c1 = Creature(random.randint(0,1000),random.randint(0,1000), "enemy.png", random.randint(10,100)) 
